3e8fa0ef983d197707fed6306ceeb3c8d003cfb8
MySendingBox <56583459+MySendingBox@users.noreply.github.com>
2021-10-29 18:42:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set +e
 
-exec node ./bin/agendash-standalone.js --db=$MONGODB_URI --collection=$COLLECTION --path=$BASE_PATH
+exec node ./bin/agendash-standalone.js 8080 --db=$MONGODB_URI --collection=$COLLECTION --path=$BASE_PATH
